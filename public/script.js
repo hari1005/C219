83b4f14d1a801be06be35549775ca9ba48ea6041
hari1005 <62102842+hari1005@users.noreply.github.com>
2022-06-25 19:51:51 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,54 +1,6 @@
 const socket = io("/");
 
-var peer = new Peer(undefined, {
-    path: "/peerjs",
-    host: "/",
-    port: "443",
-});
-
 const user = prompt("Enter your name");
-
-const myVideo = document.createElement("video");
-myVideo.muted = true;
-
-let myStream;
-
-function connectedToNewUser(userId,stream){
-    const call=peer.call(userId,stream)
-    const video=document.createElement("video")
-    call.on("stream",(userVideoStream)=>{
-        addVideoStream(video,userVideoStream)
-    })
-
-}
-
-navigator.mediaDevices
-    .getUserMedia({
-        audio: true,
-        video: true,
-    })
-    .then((stream) => {
-        myStream = stream;
-        addVideoStream(myVideo, stream);
-    })
-    socket.on("user-connected",(userId)=>{
-         connectedToNewUser(userId,stream)
-    })
-    peer.on("call",(call)=>{
-        call.answer(stream)
-        const video=document.createElement("video")
-        call.on("stream",(userVideoStream)=>{
-            addVideoStream(video,userVideoStream)
-        })
-    })
-
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
-        video.play();
-        $("#video_grid").append(video)
-    });
-};
 
 $(function () {
     $("#show_chat").click(function () {
@@ -65,60 +17,22 @@ $(function () {
     $("#send").click(function () {
         if ($("#chat_message").val().length !== 0) {
             socket.emit("message", $("#chat_message").val());
-            $("#chat_message").val("");
+            $("#chat_message").val() = "";
         }
     })
 
     $("#chat_message").keydown(function (e) {
         if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
             socket.emit("message", $("#chat_message").val());
-            $("#chat_message").val("");
+            $("#chat_message").val() = "";
         }
     })
 
-    $("#mute_button").click(function(){
-        const enabled=myStream.getAudioTracks()[0].enabled
-        if(enabled){
-            myStream.getAudioTracks()[0].enabled=false
-            html=`<i class="fas fa-microphone-slash"></i>`
-            $("#mute_button").toggleClass("background_red")
-            $("#mute_button").html("html")
-        }
-        else{
-            myStream.getAudioTracks()[0].enabled=true
-            html=`<i class="fas fa-microphone"></i>`
-            $("#mute_button").toggleClass("background_red")
-            $("#mute_button").html("html")
-        }
-    })
-    $("#stop_video").click(function(){
-        const enabled=myStream.getVideoTracks()[0].enabled
-        if(enabled){
-            myStream.getVideoTracks()[0].enabled=false
-            html=`<i class="fas fa-video-slash"></i>`
-            $("#stop_video").toggleClass("background_red")
-            $("#stop_video").html("html")
-        }
-        else{
-            myStream.getVideoTracks()[0].enabled=true
-            html=`<i class="fas fa-video"></i>`
-            $("#stop_video").toggleClass("background_red")
-            $("#stop_video").html("html")
-        }
-    })
-
-})
-
-peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id, user);
-});
-
-socket.on("createMessage", (message, userName) => {
-    $(".messages").append(`
-        <div class="message">
-            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
-        }</span> </b>
+    socket.on("create message",(message)=>{
+        $(".messages").append(
+            `<div class="message">
             <span>${message}</span>
-        </div>
-    `)
-});
+            </div>`
+        )
+    })
+})
